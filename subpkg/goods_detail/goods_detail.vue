708e94f90ea49a6a2b7b9d255180a8e707c85d66
InSeekFame <cl18083620407@163.com>
2025-03-07 13:00:08 +0800

    完成了home首页的开发

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,6 +1,6 @@
 <template>
   <view>
-    Cart
+    
   </view>
 </template>
 
@@ -9,11 +9,14 @@
     data() {
       return {
         
-      };
+      }
+    },
+    methods: {
+      
     }
   }
 </script>
 
-<style lang="scss">
+<style>
 
 </style>
